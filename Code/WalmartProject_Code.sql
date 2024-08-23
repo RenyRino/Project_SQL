cc4e4a82db80fe8a0c7cd09852fbcf6fbc00372e
Reny Rinowati <95278862+RenyRino@users.noreply.github.com>
2024-08-23 10:34:08 +0200

    Update WalmartProject_Code.sql

--- Code/WalmartProject_Code.sql
+++ Code/WalmartProject_Code.sql
@@ -1,4 +1,25 @@
-/* To Show the biggest weekly sales*/
+/* This table shows a walmart sales data. In this project I would 
+like to answear the following questions:
+1. How many stores included in this data
+2. Which date has the higest sales
+
+
+*/
+
+
+
+/* Firstly I would like to show all data of this table*/
+
+SELECT * FROM dbo.Walmart_Store_sales
+
+
+/* 1. How many stores included in this data */
+
+SELECT COUNT(DISTINCT store) FROM dbo.Walmart_Store_sales
+
+
+/* 2. Which date has the higest sales */
 
 SELECT * FROM dbo.Walmart_Store_sales
 ORDER BY Weekly_Sales DESC
+
